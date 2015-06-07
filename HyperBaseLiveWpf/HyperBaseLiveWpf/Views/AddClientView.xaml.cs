61172f89ca33dd07b1fb3096250d83907758db54
CurtisHolton1 <curtisholton1@outlook.com>
2015-06-06 21:58:25 -0400

    UI

--- HyperBaseLiveWpf/HyperBaseLiveWpf/Views/AddClientView.xaml.cs
+++ HyperBaseLiveWpf/HyperBaseLiveWpf/Views/AddClientView.xaml.cs
@@ -24,13 +24,19 @@ namespace HyperBaseLiveWpf.Views
     public partial class AddClientView : Window, INotifyPropertyChanged
     {
         private string folderBrowserText;
-        public string FolderBrowserText { get { return folderBrowserText; } set { folderBrowserText = value;  this.OnPropertyChanged("FolderBrowserText"); } }
+        public string FolderBrowserText { get { return folderBrowserText; } set { folderBrowserText = value; this.OnPropertyChanged("FolderBrowserText"); } }
+        private string clientNameText;
+        public string ClientNameText { get { return clientNameText; } set { clientNameText = value; this.OnPropertyChanged("ClientNameText"); } }
+        private string clientIDText;
+        public string ClientIDText { get { return clientIDText; } set { clientIDText = value; this.OnPropertyChanged("ClientIDText"); } }
 
         public AddClientView()
         {
             InitializeComponent();
             this.DataContext = this;
             FolderBrowserText = "Install Location";
+            ClientNameText = "Client Name";
+            ClientIDText = "Client ID";
         }
 
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
@@ -58,6 +64,55 @@ namespace HyperBaseLiveWpf.Views
         }
 
         #endregion
+
+        private void ClientNameBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (ClientNameText.Equals("Client Name"))           
+                ClientNameText = "";                     
+        }
+
+        private void FolderBrowserBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (FolderBrowserText.Equals("Install Location"))
+                FolderBrowserText = "";
+        }
+
+        private void ClientID_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (ClientIDText.Equals("Client ID"))
+                ClientIDText = "";
+        }
+
+        private void ClientNameBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (ClientNameText.Equals(""))
+                ClientNameText = "Client Name";
+        }
+
+        private void FolderBrowserBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (FolderBrowserText.Equals(""))
+                FolderBrowserText = "Install Location";
+        }
+
+        private void ClientID_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (ClientIDText.Equals(""))
+                ClientIDText = "Client ID";
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {           
+            var wnd = new InstallServiceView();
+            wnd.Show();
+            this.Close();
+        }
+
+        private void Generate_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ClientIDText = "GENERATED ID HERE";
+        }
+
 
     }
 }
